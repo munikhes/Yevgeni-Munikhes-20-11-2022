2f1e15b2ad18ea974ff01c81fe8574a9b32742c3
Yevgeni Munikhes <munikhes@gmail.com>
2022-11-24 12:21:21 +0200

    Updates

---
+++ precache-manifest.9966be5d5bdc344fcd8299cbe2fec1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93d5098834371daf370b",
+    "url": "/Yevgeni-Munikhes-20-11-2022/static/css/main.2417bd71.chunk.css"
+  },
+  {
+    "revision": "93d5098834371daf370b",
+    "url": "/Yevgeni-Munikhes-20-11-2022/static/js/main.93d50988.chunk.js"
+  },
+  {
+    "revision": "73c4a386b51143b368b7",
+    "url": "/Yevgeni-Munikhes-20-11-2022/static/js/1.73c4a386.chunk.js"
+  },
+  {
+    "revision": "fb39bb282a842bdb9707",
+    "url": "/Yevgeni-Munikhes-20-11-2022/static/js/2.fb39bb28.chunk.js"
+  },
+  {
+    "revision": "c5728d03e78cdda7e616",
+    "url": "/Yevgeni-Munikhes-20-11-2022/static/js/runtime~main.c5728d03.js"
+  },
+  {
+    "revision": "f769e504d50d7bab0bce2b135fd13077",
+    "url": "/Yevgeni-Munikhes-20-11-2022/index.html"
+  }
+];
